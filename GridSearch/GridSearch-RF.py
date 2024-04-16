ca3a67d297f1ff201c28408dd8f6dddb18f777fe
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-04-16 11:37:18 -0600

    Grid Search SVM

---
+++ GridSearch/GridSearch-RF.py
@@ -0,0 +1,68 @@
+# Import the necessary dependencies
+import multiprocess
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+# We create a list with the parameters to be evaluated 
+hyperparameters = []
+for criterion in ['gini','entropy']:
+    for trees in range(10, 100):
+        hyperparameters.append([trees, criterion])
+        
+
+def evaluate_set(hyperparameter_set, p_id, lock):
+    """
+    Evaluate a set of hyperparameters
+    Args:
+    hyperparameter_set: a list with the set of hyperparameters to be evaluated
+    """
+    import datetime
+    print('\n Yo soy el proceso', p_id, 'Comence a las',datetime.datetime.now(), "\n")
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+    # We load the dataset, here we use 80-20 for training and testing splits
+    iris=datasets.load_iris()
+    X=iris.data
+    y=iris.target
+    # se particiona el conjunto en 80-20 para la evaluación
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        stratify=y, 
+                                                        test_size=0.20)
+    for s in hyperparameter_set:
+        clf=RandomForestClassifier(n_estimators=int(s[0]), criterion=s[1])
+        clf.fit(X_train, y_train)
+        y_pred=clf.predict(X_test)
+        lock.acquire()
+        print('Accuracy en el proceso',p_id,':',accuracy_score(y_test,y_pred))
+        lock.release()
+
+
+# Now we will evaluated with more threads
+threads=[]
+N_THREADS=4
+splits=np.split(np.array(hyperparameters), N_THREADS)
+lock=multiprocess.Lock()
+for i in range(N_THREADS-1):
+    # Se generan los hilos de procesamiento
+    threads.append(multiprocess.Process(target=evaluate_set, args=[splits[i],i, lock]))
+
+
+start_time = time.perf_counter()
+# Se lanzan a ejecución
+for thread in threads:
+    thread.start()
+
+# y se espera a que todos terminen
+for thread in threads:
+    thread.join()
+            
+finish_time = time.perf_counter()
+print(f"Program finished in {finish_time-start_time} seconds")
--- GridSearch/GridSearch-SVM.py
+++ GridSearch/GridSearch-SVM.py
@@ -6,16 +6,21 @@ from sklearn.model_selection import train_test_split
 
 kernels = ['linear', 'poly', 'rbf', 'sigmoid']
 # penalización  = list(np.arange(0.1, 1, 0.1)) + list(range(1, 51)) + list(range(100, 501, 50))
-penalización  = list(np.arange(0.1, 1, 0.1)) + list(range(1, 51)) + list(range(100, 501, 50))
+penalización  = list(np.arange(0.1, 1, 0.1)) + list(range(1, 51))
 
 # tols =  [1e-3, 1e-4, 1e-5]
 tols =  [1e-3]
+
+gammas = ['scale', 'auto']
 
 hyperparameters = []
 for kernel in kernels:
     for c in penalización :
         for tol in tols :
-            hyperparameters.append([kernel, c, tol]) 
+            hyperparameters.append([kernel, c, tol])
+            if hyperparameters[0] != "linear":
+                for gamma in gammas:
+                    hyperparameters.append([kernel, c, tol,gamma])
 
 iris=datasets.load_wine()
 X=iris.data
@@ -28,7 +33,7 @@ def evaluate_set( X_train, X_test, y_train, y_test, parametros, p_id, lock):
     from sklearn.svm import SVC
     from sklearn.metrics import accuracy_score
     import datetime
-    print('Yo soy el proceso:', p_id, 'Comence a las:',datetime.datetime.now())
+    print('\n Yo soy el proceso:', p_id, 'Comence a las:',datetime.datetime.now())
     for s in parametros:
         clf=SVC(kernel=(s[0]), C=float(s[1]), tol=float(s[2]))
         clf.fit(X_train, y_train)
@@ -39,10 +44,10 @@ def evaluate_set( X_train, X_test, y_train, y_test, parametros, p_id, lock):
 
 if __name__ == '__main__':   
     threads=[]
-    N_THREADS=8
+    N_THREADS=4
     splits=np.split(np.array(hyperparameters), N_THREADS)
     lock=multiprocess.Lock()
-    for i in range(N_THREADS):
+    for i in range(N_THREADS-1):
         # Se generan los hilos de procesamiento
         threads.append(multiprocess.Process(target=evaluate_set, args=[X_train,X_test,y_train,y_test, splits[i],i, lock]))
 
